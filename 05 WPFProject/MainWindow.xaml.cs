53a250a1f331a1890286d3f528d53f8e1ffb9b37
Marcin Z <mz2018.021@outlook.com>
2019-03-07 09:38:21 +0100

    Do 1'szej przerwy 4 dzień

---
+++ 05 WPFProject/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _05_WPFProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Metoda obsługująca zdarzenie (EventHandler) przy kliknięciu. 
+        private void BtnHello_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello WPF");
+        }
+
+        private void InfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            string info = InfoText.Text;
+            InfoLabel.Content = info;
+        }
+
+        private void InfoCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboInfoText.Text = InfoCombo.SelectedValue.ToString();
+        }
+    }
+}
